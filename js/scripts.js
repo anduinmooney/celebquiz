d177dc393dad5fedd0867a3e423ac2a5d4eec7c2
Anduin Mooney and James Donlan <james@>
2017-11-30 13:39:11 -0800

    quiz working for real now, still have to add alert for not entering age

--- js/scripts.js
+++ js/scripts.js
@@ -4,29 +4,85 @@ $(document).ready(function() {
     var gender = $("select#gender").val();
     var genre = $("select#genre").val();
 
-    if (age >= 30 && 'male' && 'comedy') {
+    if (age >= 30 && gender==='male' && genre==='comedy') {
       $ ('#charlieDay').show();
+      $ ('#michaelCera').hide();
+      $ ('#amyPoehler').hide();
+      $ ('#someone').hide();
+      $ ('#keanuReeves').hide();
+      $ ('#someone1').hide();
+      $ ('#someone2').hide();
+      $ ('#someone3').hide();
     }
-    else if (age < 30 && 'male' && 'comedy') {
+    else if (age < 30 && gender==='male' && genre==='comedy') {
       $ ('#michaelCera').show();
+      $ ('#amyPoehler').hide();
+      $ ('#someone').hide();
+      $ ('#keanuReeves').hide();
+      $ ('#someone1').hide();
+      $ ('#someone2').hide();
+      $ ('#someone3').hide();
+      $ ('#charlieDay').hide();
     }
-    else if (age >= 30 && 'female' && 'comedy') {
+    else if (age >= 30 && gender==='female' && genre==="comedy") {
       $ ('#amyPoehler').show();
+      $ ('#someone').hide();
+      $ ('#keanuReeves').hide();
+      $ ('#someone1').hide();
+      $ ('#someone2').hide();
+      $ ('#someone3').hide();
+      $ ('#charlieDay').hide();
+      $ ('#michaelCera').hide();
     }
-    else if (age < 30 && 'female' && 'comedy') {
+    else if (age < 30 && gender==='female' && genre==='comedy') {
       $ ('#someone').show();
+      $ ('#keanuReeves').hide();
+      $ ('#someone1').hide();
+      $ ('#someone2').hide();
+      $ ('#someone3').hide();
+      $ ('#charlieDay').hide();
+      $ ('#michaelCera').hide();
+      $ ('#amyPoehler').hide();
     }
-    else if (age >= 30 && 'male' && 'action') {
+    else if (age >= 30 && gender==='male' && genre==='action') {
       $ ('#keanuReeves').show();
+      $ ('#someone1').hide();
+      $ ('#someone2').hide();
+      $ ('#someone3').hide();
+      $ ('#charlieDay').hide();
+      $ ('#michaelCera').hide();
+      $ ('#amyPoehler').hide();
+      $ ('#someone').hide();
     }
-    else if (age < 30 && 'male' && 'action') {
+    else if (age < 30 && gender==='male' && genre==='action') {
       $ ('#someone1').show();
+      $ ('#someone2').hide();
+      $ ('#someone3').hide();
+      $ ('#charlieDay').hide();
+      $ ('#michaelCera').hide();
+      $ ('#amyPoehler').hide();
+      $ ('#someone').hide();
+      $ ('#keanuReeves').hide();
     }
-    else if (age >= 30 && 'female' && 'action') {
+    else if (age >= 30 && gender==='female' && genre==='action') {
       $ ('#someone2').show();
+      $ ('#someone3').hide();
+      $ ('#charlieDay').hide();
+      $ ('#michaelCera').hide();
+      $ ('#amyPoehler').hide();
+      $ ('#someone').hide();
+      $ ('#keanuReeves').hide();
+      $ ('#someone1').hide();
     }
     else {
       $ ('#someone3').show();
+      $ ('#charlieDay').hide();
+      $ ('#michaelCera').hide();
+      $ ('#amyPoehler').hide();
+      $ ('#someone').hide();
+      $ ('#keanuReeves').hide();
+      $ ('#someone1').hide();
+      $ ('#someone2').hide();
     }
     event.preventDefault();
   });
